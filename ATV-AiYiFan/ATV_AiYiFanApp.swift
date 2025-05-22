10c4afe54f8664b93be44548d7f4acb92c7de43f
Tong Miao <3013204258mt@gmail.com>
2025-05-21 20:12:58 -0400

    Initial Commit

---
+++ ATV-AiYiFan/ATV_AiYiFanApp.swift
@@ -0,0 +1,17 @@
+//
+//  ATV_AiYiFanApp.swift
+//  ATV-AiYiFan
+//
+//  Created by Tong Miao on 5/21/25.
+//
+
+import SwiftUI
+
+@main
+struct ATV_AiYiFanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ATV-AiYiFanTests/ATV_AiYiFanTests.swift
@@ -0,0 +1,17 @@
+//
+//  ATV_AiYiFanTests.swift
+//  ATV-AiYiFanTests
+//
+//  Created by Tong Miao on 5/21/25.
+//
+
+import Testing
+@testable import ATV_AiYiFan
+
+struct ATV_AiYiFanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
